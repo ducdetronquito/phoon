5221f04d7ed564db42483ab02aa0a004c35bc1b1
ducdetronquito <g.paulet@zero-gachis.com>
2019-10-28 13:15:38 +0100

    :recycle: Tree - A node's children are stored in a sequence instead of a table.

--- express/routing/tree.nim
+++ express/routing/tree.nim
@@ -1,11 +1,10 @@
-import tables
 import options
 
 
 type
     Node[T] = ref object
         label*: char
-        children*: Table[char, Node[T]]
+        children*: seq[Node[T]]
         value*: Option[T]
         is_leaf: bool
 
@@ -19,18 +18,28 @@ proc new*[T](tree_type: type[Tree[T]]): Tree[T] =
     return tree_type(root: root)
 
 
+proc find_child_by_label[T](self: Node[T], label: char): Option[Node[T]] =
+    for child in self.children:
+        if child.label == label:
+            return some(child)
+
+    return none(Node[T])
+
+
 proc insert*[T](self: var Tree, path: string, value: T) =
     var current_node = self.root
 
     for character in path:
-        if current_node.children.hasKey(character):
-            current_node = current_node.children[character]
+
+        let child = current_node.find_child_by_label(character)
+        if child.isSome:
+            current_node = child.get()
             continue
 
         var node = new Node[T]
         node.label = character
 
-        current_node.children.add(character, node)
+        current_node.children.add(node)
         current_node = node
 
     current_node.value = some(value)
@@ -41,10 +50,11 @@ proc retrieve*[T](self: var Tree[T], path: string): Option[T] =
     var current_node = self.root
 
     for character in path:
-        if current_node.children.hasKey(character):
-            current_node = current_node.children[character]
-        else:
+        let child = current_node.find_child_by_label(character)
+        if child.isNone:
             return none(T)
+
+        current_node = child.get()
 
     if current_node.is_leaf:
         return some(current_node.value.get())
